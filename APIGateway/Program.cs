9baafbf2e07210a7d48bf5d33348c1e885de5766
tr-eren.ugurlu <erentrugurlu@gmail.com>
2024-08-02 17:47:41 +0300

    Ocelot Gateway configuration Compleated

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Ocelot
+builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration.AddJsonFile("ocelot.json");
 
 var app = builder.Build();
 
@@ -22,4 +29,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseOcelot().Wait();
 app.Run();
